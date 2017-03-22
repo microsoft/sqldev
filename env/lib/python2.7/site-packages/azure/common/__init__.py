898128f12f827fbc2ec14ba7a2948d88ad13be90
Meet Bhagdev <mebha@microsoft.com>
2017-03-21 17:55:30 -0700

    Added azure package

---
+++ env/lib/python2.7/site-packages/azure/common/__init__.py
@@ -0,0 +1,45 @@
+﻿#-------------------------------------------------------------------------
+# Copyright (c) Microsoft.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+
+__author__ = 'Microsoft Corp. <ptvshelp@microsoft.com>'
+__version__ = '1.1.4'
+
+
+class AzureException(Exception):
+    pass
+
+
+class AzureHttpError(AzureException):
+    def __init__(self, message, status_code):
+        super(AzureHttpError, self).__init__(message)
+        self.status_code = status_code
+
+    def __new__(cls, message, status_code, *args, **kwargs):
+        if cls is AzureHttpError:
+            if status_code == 404:
+                cls = AzureMissingResourceHttpError
+            elif status_code == 409:
+                cls = AzureConflictHttpError
+        return AzureException.__new__(cls, message, status_code, *args, **kwargs)
+
+
+class AzureConflictHttpError(AzureHttpError):
+    def __init__(self, message, status_code):
+        super(AzureConflictHttpError, self).__init__(message, status_code)
+
+
+class AzureMissingResourceHttpError(AzureHttpError):
+    def __init__(self, message, status_code):
+        super(AzureMissingResourceHttpError, self).__init__(message, status_code)
